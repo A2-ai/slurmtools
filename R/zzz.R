36887054a0dd009edade00c0df211e8edf3ee92f
Jenna Elwing <jenna@a2-ai.com>
2024-02-06 20:59:17 +0000

    automatic partitions

--- R/zzz.R
+++ R/zzz.R
@@ -12,4 +12,20 @@
   if (is.null(getOption('slurmtools.bbi_config_path'))) {
     rlang::warn("option('slurmtools.bbi_config_path') is not set. Please set it for  job submission defaults to work.")
   }
+
+  tryCatch(
+    expr = {
+      if (is.null(getOption('slurmtools.partitions'))) {
+        rlang::warn("option('slurmtools.partitions') is not set")
+      } # if
+    }, # expr
+
+    warning = {
+      options('slurmtools.partitions' = get_slurm_partitions())
+      message("option('slurmtools.partitions') was set automatically:")
+      print((options('slurmtools.partitions')))
+    } # warning
+
+  ) # tryCatch
+
 }
