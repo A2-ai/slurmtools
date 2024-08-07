99345efbce591f660b247bbd7699e5243db204ae
Matt <matthews@a2-ai.com>
2024-08-07 17:49:38 +0000

    added vignette for slurmtools

--- R/nmm-config.R
+++ R/nmm-config.R
@@ -4,8 +4,8 @@
 #' @param model_number a string of model number e.g. 101a15. Default is pulled from .mod
 #' @param files_to_track a vector of file extensions. Default is c("lst", "ext", "grd")
 #' @param tmp_dir a temporary directory location to run nonmem. Default is /tmp
-#' @param watched_dir the directory where nonmem will output. Default is ./model/nonmem
-#' @param output_dir the directory where watcher will place log and any output files default is ./model/nonmem/in_progress
+#' @param watched_dir the directory where nonmem will output. Default is here::here()/model/nonmem
+#' @param output_dir the directory where watcher will place log and any output files default is here::here()/model/nonmem/in_progress
 #' @param poll_duration the amount of time in seconds between the watchers polling
 #' @param alert Where to send alerts if any. Default is None, available options are None or Slack
 #' @param level What level to log at. Default is info. Available options are Trace, Debug, Info, Warn, Fatal
@@ -17,9 +17,9 @@
 #' @export
 #'
 #' @examples \dontrun{
-#'   generate_watcher_config(.mod)
+#'   generate_nmm_config(.mod)
 #' }
-generate_watcher_config <- function(
+generate_nmm_config <- function(
     .mod,
     model_number = "",
     files_to_track = c("lst", "ext", "grd"),
@@ -43,12 +43,19 @@ generate_watcher_config <- function(
     rlang::abort("If you want slack notifications you must also supply an email.")
   }
 
+  if (watched_dir == file.path("model", "nonmem")) {
+    watched_dir <- file.path(here::here(), watched_dir)
+  }
+  if (output_dir == file.path("model", "nonmem", "in_progress")) {
+    output_dir <- file.path(watched_dir, "in_progress")
+  }
+
   toml <- list(
     model_number = model_number,
     files_to_track = files_to_track,
     tmp_dir = tmp_dir,
-    watched_dir = file.path(here::here(), watched_dir),
-    output_dir = file.path(here::here(), output_dir),
+    watched_dir = watched_dir,
+    output_dir = output_dir,
     poll_duration = poll_duration,
     alert = alert,
     level = level,
