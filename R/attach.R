ee402440ea5e01cd5133852227e32938b5cf94e5
Matthew Smith <matthews@ip-10-2-40-93.us-east-2.compute.internal>
2024-08-27 21:00:02 +0000

    updated vignettes for different use cases.

--- R/attach.R
+++ R/attach.R
@@ -8,70 +8,57 @@
 #' slurmtools_options_message()
 #' }
 slurmtools_options_message <- function() {
-  set_header <- cli::rule(
-    left = cli::style_bold("Set slurmtools options")
-  )
-
-  unset_header <- cli::rule(
-    left = cli::style_bold("Needed slurmtools options")
-  )
-
   set_options <- c()
   unset_options <- c()
-  print_set <- FALSE
-  print_unset <- FALSE
 
+  # Check for each used options
   tmpl_path <- getOption('slurmtools.slurm_job_template_path')
   if (is.null(tmpl_path)) {
     unset_options <- c(unset_options, "option('slurmtools.slurm_job_template_path') is not set.")
-    print_unset <- TRUE
   } else {
     set_options <- c(set_options, paste("slurmtools.slurm_jon_template_path:", tmpl_path))
-    print_set <- TRUE
   }
 
   root <- getOption('slurmtools.submission_root')
   if (is.null(root)) {
     unset_options <- c(unset_options, "option('slurmtools.submission_root') is not set.")
-    print_unset <- TRUE
   } else {
     set_options <- c(set_options, paste("slurmtools.submission_root:", root))
-    print_set <- TRUE
   }
 
-  set_bullets <- paste0(
-    cli::col_green(cli::symbol$tick), " ", set_options, collapse = "\n"
-  )
-
-  unset_bullets <- paste0(
-    cli::col_red(cli::symbol$cross), " ", unset_options, collapse = "\n"
-  )
-
-  unset <- paste0(
-    cli::col_cyan(cli::symbol$info), " ",
-    cli::format_inline("Please set all options for job submission defaults to work.")
-  )
-  if (print_set) {
-    if (print_unset) {
-      paste0(
-        set_header, "\n",
-        set_bullets, "\n",
-        unset_header, "\n",
-        unset_bullets, "\n",
-        unset
-      )
-    } else {
-      paste0(
-        set_header, "\n",
-        set_bullets, "\n"
-      )
-    }
+  bbi_config <- getOption("slurmtools.bbi_config_path")
+  if (is.null(bbi_config)) {
+    unset_options <- c(unset_options, "option('slurmtools.bbi_config_path') is not set.")
   } else {
-    paste0(
-      unset_header, "\n",
-      unset_bullets, "\n",
-      unset
+    set_options <- c(set_options, paste("slurmtools.bbi_config_path:", bbi_config))
+  }
+
+  #format .onAttach message
+  msg <- ""
+  if (length(set_options)) {
+    msg <- paste0(
+      msg,
+      cli::rule(
+        left = cli::style_bold("Set slurmtools options")), "\n",
+      paste0(
+        cli::col_green(cli::symbol$tick), " ", set_options, collapse = "\n"), "\n"
     )
   }
+
+  if (length(unset_options)) {
+    msg <- paste0(
+      msg,
+      cli::rule(
+        left = cli::style_bold("Needed slurmtools options")), "\n",
+      paste0(
+        cli::col_red(cli::symbol$cross), " ", unset_options, collapse = "\n"), "\n",
+      paste0(
+        cli::col_cyan(cli::symbol$info), " ",
+        cli::format_inline("Please set all options for job submission defaults to work.")
+      )
+    )
+  }
+
+  msg
 }
 
