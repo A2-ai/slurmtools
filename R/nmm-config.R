93c2eb8b8dfb03f31f361ac320450072f785a24b
Matt Smith <matthews@a2-ai.com>
2024-09-20 19:58:07 +0000

    updated generate_nmm_config to account for new alerter and nmm config format.

--- R/nmm-config.R
+++ R/nmm-config.R
@@ -2,16 +2,14 @@
 #'
 #' @param .mod a bbi nonmem model object
 #' @param model_number a string of model number e.g. 101a15. Default is pulled from .mod
-#' @param files_to_track a vector of file extensions. Default is c("lst", "ext", "grd")
-#' @param tmp_dir a temporary directory location to run nonmem. Default is /tmp
+#' @param files_to_track a vector of file extensions. If left blank, nmm will use .ext, .lst. and .grd.
+#' @param tmp_dir a temporary directory location to run nonmem. If left blank nmm will use /tmp
 #' @param watched_dir the directory where nonmem will output. Default is here::here()/model/nonmem
 #' @param output_dir the directory where watcher will place log and any output files default is here::here()/model/nonmem/in_progress
-#' @param poll_duration the amount of time in seconds between the watchers polling
-#' @param alert Where to send alerts if any. Default is None, available options are None or Slack
-#' @param level What level to log at. Default is info. Available options are Trace, Debug, Info, Warn, Fatal
-#' @param email if alert is set to Slack, this should be the email associated with slack to get messages sent directly to you.
-#' @param threads number of threads if running a parallel job. Default is 1
-#' @param topic the ntfy.sh topic to send alerts to.
+#' @param poll_duration the amount of time in seconds between the watchers polling. If left blank, nmm will use 1 second
+#' @param level What level to log at. Available options are Trace, Debug, Info, Warn, Fatal if left blank, nmm will use Info
+#' @param threads number of threads if running a parallel job. If left blank nmm will use 1
+#' @param alerter_opts a list for setting up an alerter fields needed are alerter, command, message_flag, use_stdout, args (list of additional args to be passed to alerter)
 #'
 #' @return none
 #' @keywords internal
@@ -22,27 +20,22 @@
 #' }
 generate_nmm_config <- function(
     .mod,
-    model_number = "",
-    files_to_track = c("lst", "ext", "grd"),
-    tmp_dir = "/tmp",
+    model_number = NULL,
+    files_to_track = NULL,
+    tmp_dir = NULL,
     watched_dir = file.path("model", "nonmem"),
     output_dir = file.path(watched_dir, "in_progress"),
-    poll_duration = 1,
-    alert = "None",
-    level = "Debug",
-    email = "",
-    threads = 1,
-    topic = ""
+    poll_duration = NULL,
+    level = NULL,
+    threads = NULL,
+    alerter_opts = list()
 ) {
-  if (model_number == "") {
+  if (is.null(model_number)) {
     model_number <- basename(.mod$absolute_model_path)
   }
 
-  alert <- paste0(toupper(substring(alert, 1, 1)), substring(alert, 2))
-  level <- paste0(toupper(substring(level, 1, 1)), substring(level, 2))
-
-  if (alert == "Slack" && email == "") {
-    rlang::abort("If you want slack notifications you must also supply an email.")
+  if (!is.null(level)) {
+    level <- paste0(toupper(substring(level, 1, 1)), substring(level, 2))
   }
 
   if (watched_dir == file.path("model", "nonmem")) {
@@ -59,13 +52,25 @@ generate_nmm_config <- function(
     watched_dir = watched_dir,
     output_dir = output_dir,
     poll_duration = poll_duration,
-    alert = alert,
     level = level,
-    email = email,
-    threads = threads,
-    topic = topic
+    threads = threads
   )
 
+  toml <- purrr::compact(toml)
   config_toml_path <- paste0(.mod$absolute_model_path, ".toml")
-  write_file(rextendr::to_toml(toml), config_toml_path)
+  alerter_opts <- purrr::compact(alerter_opts)
+
+  if (length(alerter_opts) != 0) {
+    if ("args" %in% names(alerter_opts)) {
+      write_file(rextendr::to_toml(
+        toml,
+        alerter = alerter_opts[names(alerter_opts) != "args"],
+        alerter.args = alerter_opts$args),
+        config_toml_path)
+    } else {
+      write_file(rextendr::to_toml(toml, alerter = alerter_opts), config_toml_path)
+    }
+  } else {
+    write_file(rextendr::to_toml(toml), config_toml_path)
+  }
 }
